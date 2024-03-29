8ed758d6846507750026a4b2ae88b60bdbafccde
rachmatsasongko <rachmatsasongko@gmail.com>
2024-03-30 05:14:06 +0700

    update init-main

--- init-mainnet.sh
+++ init-mainnet.sh
@@ -4,10 +4,9 @@
 VALIDATOR_NAME=validator1
 CHAIN_ID=rcm
 KEY_NAME=rcm-key
-KEY_2_NAME=rcm-key-2
 CHAINFLAG="--chain-id ${CHAIN_ID}"
-TOKEN_AMOUNT="10000000000000000000000000arcomia"
-STAKING_AMOUNT="1000000000arcomia"
+TOKEN_AMOUNT="10000000000000000000000000stake"
+STAKING_AMOUNT="1000000000stake"
 
 # query the DA Layer start height, in this case we are querying
 # an RPC endpoint provided by Celestia Labs. The RPC endpoint is
@@ -16,6 +15,9 @@ STAKING_AMOUNT="1000000000arcomia"
 # network. This is for Arabica, if using another network, change the RPC.
 DA_BLOCK_HEIGHT=$(curl https://rpc.lunaroasis.net/block | jq -r '.result.block.header.height')
 echo -e "\n Your DA_BLOCK_HEIGHT is $DA_BLOCK_HEIGHT \n"
+
+AUTH_TOKEN=$(celestia light auth write --p2p.network celestia)
+echo -e "\n Your DA AUTH_TOKEN is $AUTH_TOKEN \n"
 
 # build the rcm chain with Rollkit
 ignite chain build
@@ -26,16 +28,14 @@ rcmd tendermint unsafe-reset-all
 # initialize the validator with the chain ID you set
 rcmd init $VALIDATOR_NAME --chain-id $CHAIN_ID
 
-# add keys for key 1 and key 2 to keyring-backend test
+# add keys for key 1 to keyring-backend test
 rcmd keys add $KEY_NAME --keyring-backend test
-rcmd keys add $KEY_2_NAME --keyring-backend test
 
 # add these as genesis accounts
 rcmd genesis add-genesis-account $KEY_NAME $TOKEN_AMOUNT --keyring-backend test
-rcmd genesis add-genesis-account $KEY_2_NAME $TOKEN_AMOUNT --keyring-backend test
 
 # set the staking amounts in the genesis transaction
-rcmd genesis gentx $KEY_NAME $STAKING_AMOUNT --chain-id $CHAIN_ID --keyring-backend test
+# rcmd genesis gentx $KEY_NAME $STAKING_AMOUNT --chain-id $CHAIN_ID --keyring-backend test
 
 # collect genesis transactions
 rcmd genesis collect-gentxs
@@ -49,8 +49,9 @@ jq --argjson pubKey "$PUB_KEY" '.consensus["validators"]=[{"address": "'$ADDRESS
 # create a restart-mainnet.sh file to restart the chain later
 [ -f restart-mainnet.sh ] && rm restart-mainnet.sh
 echo "DA_BLOCK_HEIGHT=$DA_BLOCK_HEIGHT" >> restart-mainnet.sh
+echo "AUTH_TOKEN=$AUTH_TOKEN" >> restart-mainnet.sh
 
-echo "rcmd start --rollkit.aggregator --rollkit.da_address=":26650" --rollkit.da_start_height \$DA_BLOCK_HEIGHT --rpc.laddr tcp://127.0.0.1:36657 --grpc.address 127.0.0.1:9290 --p2p.laddr \"0.0.0.0:36656\" --minimum-gas-prices="0.025arcomia"" >> restart-mainnet.sh
+echo "rcmd start --rollkit.aggregator --rollkit.da_auth_token=\$AUTH_TOKEN --rollkit.da_namespace 3d3528ddb5145a57c7eb37b6d50deec11e29b4c93732f7bb0227f23ff942 --rollkit.da_start_height \$DA_BLOCK_HEIGHT --rpc.laddr tcp://127.0.0.1:36657 --grpc.address 127.0.0.1:9290 --p2p.laddr \"0.0.0.0:36656\" --minimum-gas-prices="0.025stake"" >> restart-mainnet.sh
 
 # start the chain
-rcmd start --rollkit.aggregator --rollkit.da_address=":26650" --rollkit.da_start_height $DA_BLOCK_HEIGHT --rpc.laddr tcp://127.0.0.1:36657 --grpc.address 127.0.0.1:9290 --p2p.laddr "0.0.0.0:36656" --minimum-gas-prices="0.025arcomia"
+rcmd start --rollkit.aggregator --rollkit.da_auth_token=$AUTH_TOKEN --rollkit.da_namespace 3d3528ddb5145a57c7eb37b6d50deec11e29b4c93732f7bb0227f23ff942 --rollkit.da_start_height $DA_BLOCK_HEIGHT --rpc.laddr tcp://127.0.0.1:36657 --grpc.address 127.0.0.1:9290 --p2p.laddr "0.0.0.0:36656" --minimum-gas-prices="0.025stake"
